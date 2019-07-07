a6b935ed3873ce99c6f1d33c8c275a639c7710ed
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-07-07 15:45:41 -0700

    Post req is working in postman but not when form is submitted

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,7 @@
 // Here we import  data in friends.js
 var candidates = require ("../data/friends.js");
 module.exports=function(app){
-    console.log("api rounts connected!")
+    
   // Displays all candidates
   app.get("/api/candidates", function(req, res){
     res.json(candidates);
@@ -21,34 +21,33 @@ module.exports=function(app){
     // This works because of our body parsing middleware
     
     var newcandidate = req.body;
-    console.log(newcandidate);
-    // Using a RegEx pattern to remove spaces fro newcandidate 
+    // console.log("this is the new candidates name " + newcandidate.name);
+    // console.log("this is the new candidates name " + newcandidate);
+
+    // Using a RegEx pattern to remove spaces from newcandidate 
     // More informaiton is available at https://www.regexbuddy.com/regex.html
-    newcandidate.name = newcandidate.name.replace(/\s+/g, "").toLowerCase();
+   
+   // I DON'T THINK THIS LINE OF CODE IS NEEDED
+    // newcandidate.name = newcandidate.name.replace(/\s+/g, "").toLowerCase();
     
-    console.log(newcandidate);
+    
     // Loop through the FriendNeeded array and candidates array and determine the match. The variable score is the difference in the answers between the two arrays
     // for (var j = 0; j < candidates.length; j++){
     //   var score =0;
     //   for (var i =0; i<10; i++){
-    //     score = score + Math.abs((FriendNeeded.scores[i]-candidates[j].scores[i]));
+    //     score = score + Math.abs((newcandidate.scores[i]-candidates[j].scores[i]));
     //   }
     //   // create an array of objects that will provide the name and the match number  )
     //   console.log(score);
     // }
     
     candidates.push(newcandidate);
+    console.log(candidates.name)
+    
     
     res.json(newcandidate);
   })
   
-  // Determin who is compatible
-  // Create variable of person looking for a friend. This is created to test the program. When its working the information will be entered dynamically
-  // var FriendNeeded = {
-  //   "name": "Barney",
-  //   "photo": "",
-  //   "scores": [1,4,3,2,1,1,2,3,4,5]
-  // }
   
   
 }
